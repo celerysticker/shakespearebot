9ab6fd6978efe11c1ffefac429445a159590b227
Nancy <nwen2011@gmail.com>
2016-03-08 23:54:52 -0800

    Fixed indentation issue

--- preprocess_words_only.py
+++ preprocess_words_only.py
@@ -4,30 +4,32 @@ Created on Tue Mar  8 23:37:56 2016
 
 @author: nancywen
 """
-
-import nltk
+from nltk.tokenize import RegexpTokenizer
 
 # Run this line the first time
 # nltk.download('punkt')
 
 def tokenize_lines_words_only(filename):
-	all_tokens = []
-	line_count = 14
-	i = 0
-	with open(filename) as f:
-		for line in f.readlines():
-			line = line.strip()
-			try:
-				int(line)
-				i = 0 # reset line count
-				continue
-			except ValueError:
-				pass
-
-			if len(line) > 0:
-				tokens = nltk.word_tokenize(line)
-				all_tokens.append(tokens)
-			i += 1
-	return all_tokens
-
+""" Creates a list of tokens out of the poems that only contains
+    the words (no punctuation) """
+    all_tokens = []
+    line_count = 14
+    i = 0
+    tokenizer = RegexpTokenizer(r'\w+')
+    with open(filename) as f:
+        for line in f.readlines():
+            line = line.strip()
+            try:
+                int(line)
+                i = 0
+                continue
+            except ValueError:
+                pass
+            if len(line) > 0:
+                tokens = tokenizer.tokenize(line)
+                all_tokens.append(tokens)
+            i += 1
+    return all_tokens
+    
+    
 print tokenize_lines_words_only('data/shakespeare.txt')
