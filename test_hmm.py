69daef94eda2404c84fc697da901d6e3c42e58d2
Nancy <nwen2011@gmail.com>
2016-03-11 13:43:52 -0800

    Create tester for hmm

---
+++ test_hmm.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 13:41:37 2016
+
+@author: nancywen
+"""
+
+from hmm import train_hmm
+
+def main():
+    # Correct A and O matrix after training
+
+    # Train on test set
+
+    # Compare trained HMM to correct HMM
+
+    pass
+
+if __name__ == "__main__":
+    main()
