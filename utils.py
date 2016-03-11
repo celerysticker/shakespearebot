c3777f3aaa686956451c8ef94d9b00b5618c7f01
Christina Lin <cristynalin@gmail.com>
2016-03-11 15:06:44 -0800

    Added Shakespearen word syllable count file

--- utils.py
+++ utils.py
@@ -3,12 +3,20 @@ import curses
 import nltk
 from nltk.corpus import cmudict
 import preprocess_words_only as pp
+import json
 
 # Run this line the first time
 # nltk.download('cmudict')
 # if this doesn't work, try running nltk.download() in commandline
 
 d = cmudict.dict()
+
+# syllables only
+with open("shakespeare_syl.json") as df:
+	shakespeare_syl = json.load(df)
+
+# break down each word into phonemes
+shakespeare_dict = {}
 
 # not sure why curses.ascii.isdigit doesn't work :(
 def isdigit(d):
@@ -22,7 +30,10 @@ def nsyl(word):
 	"""Calculates syllable count of single word"""
 	lowercase = word.lower()
 	if lowercase not in d:
-		return -1
+		if lowercase not in shakespeare_syl:
+			return 0 # skip word
+		else:
+			return shakespeare_syl[word]
 	else:
 		return max([len([y for y in x if isdigit(y[-1])]) for x in d[lowercase]])
 
@@ -35,7 +46,21 @@ def get_syllables_str(line):
 		syllables += nsyl(word)
 	return syllables
 
-# if __name__ == "__main__":
-#	# for testing
-# 	print nsyl("arithmetic")
-# 	print get_syllables_str("This is: a poem.")
+def get_shakespeare_words(filename):
+	words = []
+	lines = pp.tokenize_lines_words_only(filename)
+	for line in lines:
+		for word in line:
+			word = word.lower()
+			if word not in d:
+				words.append(word)
+	# print len(words)
+	# print len(list(set(words)))
+	return sorted(list(set(words)))
+
+if __name__ == "__main__":
+	# for testing
+	pass
+
+	# print nsyl("arithmetic")
+	# print get_syllables_str("This is: a poem.")
