2c270d460649656b0021fd256f341e2f97070363
Christina Lin <cristynalin@gmail.com>
2016-03-13 13:46:33 -0700

    Added probabilistic random picker

--- generate_line.py
+++ generate_line.py
@@ -56,6 +56,16 @@ def get_emissions(state_seq, OM):
         emission.append(max_obs)
     return emission
 
+def random_pick(some_list, probabilities):
+    ''' Probabilistic random picking'''
+    x = random.uniform(0, 1)
+    cumulative_probability = 0.0
+    for item, item_probability in zip(some_list, probabilities):
+        cumulative_probability += item_probability
+        if x < cumulative_probability: 
+            break
+    return item
+
 def prettyprint(arr):
     print ' '.join(arr)
 
